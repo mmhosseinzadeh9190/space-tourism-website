0a709875c297b40087066727e4cc7683c16bfa34
Mohammad Mahdi Hosseinzadeh <mohammadmahdihosseinzadeh68@gmail.com>
2024-06-05 00:05:59 +0330

    chore: added hamburger menu & its functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
 import "../css/style.css";
+
+const btnNav = document.querySelector(".mobile-nav");
+const header = document.querySelector(".header");
+
+btnNav.addEventListener("click", function () {
+  header.classList.toggle("nav-open");
+});
